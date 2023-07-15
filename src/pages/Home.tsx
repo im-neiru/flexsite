b4e2b7e21ea4ee90e61f99b9b3a94a2a6985450b
neilcantorne <cantorne.neil@gmail.com>
2023-07-15 10:26:45 +0800

    feat: attach `TypingText` to `Home`

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { TypingText } from "../components";
 import "../scss/home.scss";
 
 export function Home() {
@@ -6,6 +7,17 @@ export function Home() {
       <div class="introduction">
         <h2>Hi, I'am</h2>
         <h1>Neil Cantorne</h1>
+        <TypingText
+          texts={[
+            "Web Developer",
+            "ReactJS",
+            "Svelte",
+            "SolidJS",
+            "Typescript",
+            "Rust",
+            "Python"
+          ]}
+        />
       </div>
     </div>
   );
