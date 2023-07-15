94da1b068a4f5715075a47c293dd050ce1ad2e14
neilcantorne <cantorne.neil@gmail.com>
2023-07-15 09:08:45 +0800

    feat: add introduction

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,12 @@
+import "../scss/home.scss";
+
 export function Home() {
-  return <></>;
+  return (
+    <div class="home-box">
+      <div class="introduction">
+        <h2>Hi, I'am</h2>
+        <h1>Neil Cantorne</h1>
+      </div>
+    </div>
+  );
 }
