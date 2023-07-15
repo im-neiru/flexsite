546d4613efb1e58d7274749951e4cd77799ff9cb
neilcantorne <cantorne.neil@gmail.com>
2023-07-15 13:17:11 +0800

    feat: add `Router` to `App`

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import type { Component } from "solid-js";
-import { Home } from "./pages";
+import { About, Home, Contact } from "./pages";
 import { HyperBackground } from "./components";
+import { Route, Router, Routes } from "@solidjs/router";
 
 const App: Component = () => {
   return (
     <>
       <HyperBackground />
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+        </Routes>
+      </Router>
     </>
   );
 };
