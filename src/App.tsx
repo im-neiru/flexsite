92fd75d117f2a8af1508f3e5a3c9b126720cb1df
neilcantorne <cantorne.neil@gmail.com>
2023-07-15 14:51:06 +0800

    feat: add `NavigationBar` items to `App`

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
 import type { Component } from "solid-js";
 import { About, Home, Contact } from "./pages";
-import { HyperBackground } from "./components";
+import { HyperBackground, NavigationBar } from "./components";
 import { Route, Router, Routes } from "@solidjs/router";
+import iconHome from "./assets/icon-home.svg";
+import iconAbout from "./assets/icon-about.svg";
+import iconContact from "./assets/icon-contact.svg";
 
 const App: Component = () => {
   return (
     <>
       <HyperBackground />
       <Router>
+        <NavigationBar
+          routes={[
+            {
+              name: "Home",
+              path: "/",
+              icon: iconHome
+            },
+            {
+              name: "About",
+              path: "/about",
+              icon: iconAbout
+            },
+            {
+              name: "Contact",
+              path: "/contact",
+              icon: iconContact
+            }
+          ]}
+        />
         <Routes>
           <Route path="/" component={Home} />
           <Route path="/about" component={About} />
